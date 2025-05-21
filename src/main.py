662be98829734e6869b822808c3a03c702b7f43d
lucasandresc <lucas.andres.cabrera@gmail.com>
2025-05-21 20:23:14 -0300

    B: Added main.py and main.sh to run the code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+def main():
+    example = TextNode("This is some anchor text", "link", "http://www.boot.dev")
+    print(repr(example))
+
+
+
+
+if __name__=="__main__":
+    main()
